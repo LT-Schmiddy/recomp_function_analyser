0d08e630844a603804ac9647ea44871bd2fdb63f
danielryb <59661841+danielryb@users.noreply.github.com>
2024-12-21 02:54:39 +0100

    fix initial testing code

--- pyc_test.py
+++ pyc_test.py
@@ -4,14 +4,24 @@ from pycparser import parse_file
 filename = 'mm/src/libultra/os/gettime.c'
 
 include_dirs = [
-    "mm/include"
+    'mm/include',
+    'mm/src',
+    'mm/assets'
+]
+
+cpp_flags = [
+    '-U__GNUC__',
+    '-nostdinc',
+    '-E',
+    '-D_LANGUAGE_C',
+    '-DMIPS',
 ]
 
 # Attempting Analysis:
 clang_path = shutil.which("clang")
 ast = parse_file(filename, use_cpp=True,
         cpp_path=clang_path,
-        cpp_args=['-E'] + [
-            f"-I{i}" for i in include_dirs
+        cpp_args=cpp_flags + [
+            f'-I{i}' for i in include_dirs
         ])
 ast.show()
